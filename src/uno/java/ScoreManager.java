a1feb0af2ccf3816d83f7e69f47ef54f5fe04583
MaestroChicken <cxj8010@gmail.com>
2026-04-24 01:41:51 +1200

    Updated ScoreManager

--- src/uno/java/ScoreManager.java
+++ src/uno/java/ScoreManager.java
@@ -1,5 +1,35 @@
 package uno.java;
 
+import java.util.*;
+
 public class ScoreManager {
     // stub for now
+    public ScoreManager() {}
+    
+    public int calculatePoints(List<Player> players, Player winner) {
+        int total = 0;
+        for (Player p : players) {
+            if (p == winner) continue;
+            for (Card c : p.getHand()) {
+                total += pointsForCard(c);
+            }
+        }
+        return total;
+    }
+
+    public int pointsForCard(Card c) {
+        switch (c.getType()) {
+            case NORMAL:
+                return c.getNumber();
+            case SKIP:
+            case REVERSE:
+            case DRAW_TWO:
+                return 20;
+            case WILD:
+            case DRAW_FOUR:
+                return 50;
+            default:
+                return 0;
+        }
+    }
 }
